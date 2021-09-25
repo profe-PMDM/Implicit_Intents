4e4238821bd09063f0e0f4d8e437b7448bd4b9a9
David Corcuera <david.corcuera@gmail.com>
2021-09-25 13:26:03 +0200

    Prepare View Binding

--- app/src/main/java/es/jesuitas/dam/implicitintents/MainActivity.kt
+++ app/src/main/java/es/jesuitas/dam/implicitintents/MainActivity.kt
@@ -2,10 +2,13 @@ package es.jesuitas.dam.implicitintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import es.jesuitas.dam.implicitintents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
